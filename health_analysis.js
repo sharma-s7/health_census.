1aa47864ad0cc576e044f3d689563e70980946f1
sharma-s7 <shubhamsharmadev9@gmail.com>
2025-08-23 11:28:51 -0400

    Update

--- health_analysis.js
+++ health_analysis.js
@@ -58,9 +58,43 @@ function addPatient() {
     for (const gender in genderConditionsCount) {
       report.innerHTML += `${gender}:<br>`;
       for (const condition in genderConditionsCount[gender]) {
-        report.innerHTML += `&nbsp;&nbsp;${condition}: ${genderConditionsCount[gender][condition]}<br>`;
+        report.innerHTML += `&nbsp;&nbsp;${condition}: ${genderConditionsCount[gender][condition]}<br>`; //  &nbsp;&nbsp; adds two non-breaking spaces—useful for formatting or indentation in HTML output.
+
       }
     }
   }
 
 addPatientButton.addEventListener("click", addPatient);
+
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('health_analysis.json')
+        .then(response => response.json())
+        .then(data => {
+            const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+            if (condition) {
+                const symptoms = condition.symptoms.join(', ');
+                const prevention = condition.prevention.join(', ');
+                const treatment = condition.treatment;
+
+                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+                resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+                resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+                resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+            } else {
+                resultDiv.innerHTML = 'Condition not found.';
+            }
+        })
+         .catch(error => {
+            console.error('Error:', error);
+            resultDiv.innerHTML = 'An error occured while fetching data. ';
+         });
+}
+
+btnSearch.addEventListener('click', searchCondition);
